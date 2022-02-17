b0f03746fa4701d6dab50f57d863f5f1c4936847
band0x <98318897+band0x@users.noreply.github.com>
2022-02-17 11:58:36 +1100

    Create SetupBackup.sol

---
+++ SetupBackup.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.0;
+// Imports
+
+
+
+
+// Deploys Flora Token and checks to see if balance == 0 to be solved
+contract SetupBackup  {
+    
+    function isSolved() external view returns (bool) {
+        
+    }
+}
